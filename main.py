2b233d32fa4c7f5918e91c8bc7f4f16389282b3a
komkrichbuathong-cpu <komkrichbuathong@gmail.com>
2026-05-19 05:14:43 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online: {bot.user}")
+
+bot.run(os.getenv("TOKEN"))
